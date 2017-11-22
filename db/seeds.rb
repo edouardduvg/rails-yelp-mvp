f2be4a868703e814a941bd9a2bb8d0d7b1d201d9
edouard duverger <edouard.courtois.duverger@gmail.com>
2017-11-22 17:47:55 +0100

    Exercice 1 on help, lacking style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '0142241010',
+    category:        'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '0226343567',
+    category:        'italian'
+  },
+  {
+    name:         'Boabaj',
+    address:      '4 rue Saintgre;jhgsqd',
+    phone_number:  '0146241010',
+    category:        'japanese'
+  },
+  {
+    name:         'Japon in ret',
+    address:      '5 rue jhgdhjdz London E1 6PQ',
+    phone_number:  '0255343567',
+    category:        'japanese'
+  },
+  {
+    name:         'litlle italy',
+    address:      '5 rue oberkampfQ',
+    phone_number:  '0255343567',
+    category:        'italian'
+  },
+
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
